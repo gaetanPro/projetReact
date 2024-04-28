17548165f6828301d837c3e641c513656872046e
Gaetan Bagze <gaetanbagze995@gmail.com>
2024-04-28 21:59:59 +0200

    dinosaure volant
    implementation de la partie du dinosaure volant

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-
-
 function calculeDiscriminant() {
 
 let a = document.getElementById('a').value;
 let b = document.getElementById('b').value;
 let c = document.getElementById('c').value;
-let discriminant =(b*b) - (4*(a*c));
+const discriminant =(b*b) - (4*(a*c));
 
 
-    if (discriminant < 0) {
-   console.log("le discriminant est", discriminant);
+      if (discriminant < 0) {
+        let EltDplaces = document.querySelector('.ElementVolant');
+        let zoneCalcule =  document.querySelector(".calculons");
+        let zone1 = document.querySelector(".zone2");
+        zone1.style.display='block'
+        zoneCalcule.style.display = "none";
+        //alert(discriminant +" est la valeur du discriminant");
+        EltDplaces.style.backgroundColor = "orange";
         console.log("Racines complexes dans l'ensemble C des nombres complexes.");
         // Calculer les parties réelles et imaginaires des racines complexes
         const realPart = -b / (2 * a);
@@ -18,6 +22,18 @@ let discriminant =(b*b) - (4*(a*c));
         // Afficher les racines complexes
         console.log("Racine 1:", realPart + " + " + imaginaryPart + "i");
         console.log("Racine 2:", realPart + " - " + imaginaryPart + "i");
+        const normeRacine1 = Math.sqrt(Math.pow(realPart, 2)+Math.pow(imaginaryPart, 2));
+        const normeRacine2 = Math.sqrt(Math.pow(realPart, 2)+Math.pow(imaginaryPart, 2));
+        const norme=Math.floor(normeRacine1+normeRacine2);
+        let u =norme/10;
+        EltDplaces.style.animationDuration= norme+"s";
+        
+        document.body.style.backgroundColor = "lightblue";
+        normeRacine1 = 2
+       
+        let paragrap = document.createElement("p");
+        paragrap.textContent = " Norme de la racine 1: " + normeRacine1 + " Norme de la racine 2: " + normeRacine2;
+        document.body.appendChild(paragrap);
     
         // Comme il n'y a pas de racine réelle, la norme n'est pas définie
         console.log("Norme non définie pour des racines complexes.");
@@ -26,11 +42,11 @@ let discriminant =(b*b) - (4*(a*c));
         let zoneCalcule =  document.querySelector(".calculons");
         let zone1 = document.querySelector(".zone1");
         zone1.style.display='block'
-       zoneCalcule.style.display = "none";
+        zoneCalcule.style.display = "none";
         let racine1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-        const racine2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        let racine2 = (-b - Math.sqrt(discriminant)) / (2 * a);
         let r = Math.floor(racine1)
-       alert(discriminant)
+        //alert(discriminant)
         EltDplaces.style.backgroundColor = "orange";
         racine1 =2
         let t = racine1; // Assuming 't' is a variable containing the animation duration in seconds
